5482938d2c8d7277cee61b9a0f8dc4dbba673659
sergeykonar <sergey.konar@qubixstudio.com>
2022-09-02 15:21:35 +0200

    test commit

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.composetutorial
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -40,6 +41,7 @@ import kotlin.random.Random
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("TEST", "ed")
         setContent {
 
             Scaffold(topBar = {
